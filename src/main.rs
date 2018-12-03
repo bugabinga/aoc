14e3426460660ba382d3edc45b00cab9a20f75fd
Oliver Jan Krylow <oliver@bugabinga.net>
2018-12-03 16:41:20 +0100

    Day 2 of AoC. Part One complete! Two TODO.

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,13 @@
 //! The main program simply prints the results of all solved challenges so far.
 
 mod day_one;
+mod day_two;
 
 /// Print the day of the challenge and the result per line of all solved challenges so far.
 fn main() {
     println!("Day 1 Part 1: {}", day_one::part_one());
     println!("Day 1 Part 2: {}", day_one::part_two());
+
+    println!("Day 2 Part 1: {}", day_two::part_one());
+    println!("Day 2 Part 2: {}", day_two::part_two());
 }
