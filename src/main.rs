46762d0e17bc9a4e3c51f3548eaa12859eaa4f41
Oliver Jan Krylow <oliver@bugabinga.net>
2018-12-03 12:56:39 +0100

    Day 1 of AoC!!!

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+//! Advent of Code 2018
+//! Oliver Jan Krylow
+//! Rust learning exercise
+//!
+//! AoC poses a cute programming challenge each day of December until the 24th.
+//! Each daily challenge gets solved in its own rust module.
+//! The main program simply prints the results of all solved challenges so far.
+
+mod day_one;
+
+/// Print the day of the challenge and the result per line of all solved challenges so far.
+fn main() {
+    println!("Day 1 Part 1: {}", day_one::part_one());
+    println!("Day 1 Part 2: {}", day_one::part_two());
+}
